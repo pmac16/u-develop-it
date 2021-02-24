dd178a2da933a463e9e1c2c5ed47155d83cf73d4
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-02-23 18:25:09 -0800

    add post route

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const inputCheck = require('./utils/inputCheck');
+
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -17,10 +19,22 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
 });
 
-// Request a list of all potential candidates
-// db.all(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+//Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if(err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+
+        res.json({
+            message:'success',
+            data:rows
+        });
+    });
+});
 
 // GET a single candidate
 app.get('/api/candidate/:id', (req,res) => {
@@ -55,35 +69,31 @@ app.delete('/api/candidate/:id', (req,res) => {
     });
 });
 
-
-//Get all candidates
-app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
+//Create a candidate
+app.post('/api/candidate', ({body}, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({error:errors});
+        return;
+    }
+    const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+    VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    //ES5 function, not arrow function, to use this
+    db.run(sql, params, function(err, result) {
         if(err) {
-            res.status(500).json({error: err.message});
+            res.status(400).json({error:err.message});
             return;
-        }
-
-        res.json({
-            message:'success',
-            data:rows
-        });
+    }
+    res.json({
+        message:'success',
+        data: body,
+        id: this.lastID
+    })
     });
 });
 
-//Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-//ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-    if(err) {
-        console.log(err);
-    }
-    console.log(result, this.lastID);
-})
+
 
 //Last one 
 //Default resposne for any other request (Not Found) Catch all
